388de5ff5118411e32d6fe56ef99d70b2ecf82a8
Robert Kroeger <rjkroege@chromium.org>
2014-03-28 15:38:00 -0400

    added some unit tests for window

--- window/window_test.go
+++ window/window_test.go
@@ -3,10 +3,34 @@ package window
 import (
 	"github.com/rjkroege/wikitools/testhelpers"
 	"testing"
+	"image"
+	"code.google.com/a/google.com/p/gojira/content"
 )
+
+// Mock event handler.
+type mockeventhandler struct {
+	
+}
+
+func (f *mockeventhandler) Mousedown(pt image.Point, button, buttons uint32) uint32 {
+	return content.EVD_NON
+}
+
+func (f *mockeventhandler) Mouseup(pt image.Point, button, buttons  uint32) uint32 {
+	return content.EVD_NON
+}
+
+func (f *mockeventhandler) Mousemove(pt image.Point, buttons  uint32) uint32 {
+	return content.EVD_NON
+}
+
+func (f *mockeventhandler) Wheel(pt image.Point, buttons  uint32, dx, dy, dz float32) uint32 {
+	return content.EVD_NON
+}
 
 func Test_windowCreation(t *testing.T) {
 	w := new(Window)
+	w.ev = new(mockeventhandler)
 	testhelpers.AssertInt(t, 0, int(w.width))
 	testhelpers.AssertInt(t, 0, int(w.height))
 	testhelpers.AssertInt(t, 0, int(w.width))
@@ -17,6 +41,7 @@ func Test_windowCreation(t *testing.T) {
 
 func Test_onMouseBtn(t *testing.T) {
 	w := new(Window)
+	w.ev = new(mockeventhandler)
 
 	testhelpers.AssertInt(t, 1, MOUSE_BUTTON_LEFT)
 	testhelpers.AssertInt(t, 2, MOUSE_BUTTON_MIDDLE)
@@ -40,8 +65,9 @@ func Test_onMouseBtn(t *testing.T) {
 
 func Test_onMousePos(t *testing.T) {
 	w := new(Window)
+	w.ev = new(mockeventhandler)
+
 	w.onMousePos(1, 3)
 	testhelpers.AssertInt(t, 1, int(w.pointer.x))
 	testhelpers.AssertInt(t, 3, int(w.pointer.y))
-
 }
